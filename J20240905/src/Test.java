eb4f82df4f21d3668dc8a2be844b2123d30976a4
root <2762009616@qq.com>
2024-09-16 13:47:30 +0800

    test

---
+++ J20240905/src/Test.java
@@ -0,0 +1,10 @@
+//这是测试文件
+public class Test {
+    public static void main(String[] args) {
+        int num = 0;
+        for (int i = 0; i < 10; i++) {
+            num++;
+        }
+        System.out.println("运行正常 " + num);
+    }
+}
